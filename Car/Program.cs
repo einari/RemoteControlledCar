13ca4a4464f508e705d03de55b94727dee4adab7
Einar Ingebrigtsen <einar@dolittle.com>
2014-04-19 12:28:08 +0200

    Cleaning up - no need for the designer!

--- Car/Program.cs
+++ Car/Program.cs
@@ -1,36 +1,45 @@
-﻿using NETMF.OpenSource.XBee;
-using NETMF.OpenSource.XBee.Api;
-using System.Threading;
-using System.Text;
-using System;
-using Microsoft.SPOT;
+﻿using System.Text;
 using Gadgeteer.Modules.GHIElectronics;
+using GHIElectronics.Gadgeteer;
+using Microsoft.SPOT;
+using Modules;
+using NETMF.OpenSource.XBee;
+using NETMF.OpenSource.XBee.Api;
+using XBee = Gadgeteer.Modules.OpenSource.XBee;
 
 namespace Car
 {
-    public partial class Program
+    public class Program : Gadgeteer.Program
     {
-        void ProgramStarted()
+        FEZCerberus _mainboard;
+        XBee _xbee;
+        MotorControllerL298 _motorController;
+        CharacterDisplay _characterDisplay;
+
+
+        public Program(FEZCerberus mainboard)
         {
-            xBee.DebugPrintEnabled = true;
-
-            xBee.Configure();
-            //xBee.Enable();
-            xBee.Api.DataReceived += Api_DataReceived;
-
-            var timer = new Gadgeteer.Timer(1000);
-
-            timer.Tick += timer_Tick;
-
-            timer.Start();
-
-            xBee.Api.Send("Hello from the car").ToAll().Invoke();
-        }
-
-        void timer_Tick(Gadgeteer.Timer timer)
-        {
+            _mainboard = mainboard;
+            _characterDisplay = new CharacterDisplay(1, 0x20);
+            _characterDisplay.TurnOnBacklight();
+            _characterDisplay.SetCursor(0, 0);
+            _characterDisplay.PrintString("Starting up");
             
+
+
+            _xbee = new Gadgeteer.Modules.OpenSource.XBee(6);
+            _motorController = new Gadgeteer.Modules.GHIElectronics.MotorControllerL298(4);
+
+            _xbee.DebugPrintEnabled = true;
+
+            _xbee.Configure();
+            //xBee.Enable();
+            _xbee.Api.DataReceived += Api_DataReceived;
+            _xbee.Api.Send("Hello from the car").ToAll().Invoke();
+
+            _characterDisplay.PrintString("Started");
         }
+
 
         void Api_DataReceived(XBeeApi receiver, byte[] data, XBeeAddress sender)
         {
@@ -40,9 +49,17 @@ namespace Car
             var speed = (int)(y*70.0);
 
             Debug.Print(message);
-            motorControllerL298.MoveMotor(MotorControllerL298.Motor.Motor1, speed);
+            _motorController.MoveMotor(MotorControllerL298.Motor.Motor1, speed);
+        }
 
-            
+
+        public static void Main()
+        {
+            var mainboard = new FEZCerberus();
+            Gadgeteer.Program.Mainboard = mainboard;
+
+            var program = new Program(mainboard);
+            program.Run();
         }
     }
 }
